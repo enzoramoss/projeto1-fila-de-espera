25be409e911d27551c65282c92377e266592fe22
Enzo Ramos <ramosenzo.3101@gmail.com>
2024-10-21 15:49:10 -0300

    (script.js) MELHORIAS NA CONSTRUÇÃO DO CÓDIGO
    Pequenas mudanças que deixam o código melhor, mais bonito e mais compreensível:
    MENU trocado de if else para switch case

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 let queue = []
 let option = true
 
-while (option !== `3`) {
-    let menu = `Fila de espera:`
+while (option !== '3') {
+    let menu = 'Fila de espera:\n'
 
     if (queue.length === 0) {
-        menu += `Nenhum paciente na fila.\n`
+        menu += 'Nenhum paciente na fila.\n'
     } else {
         for (let i = 0; i < queue.length; i++) {
             menu += `${i + 1}º ${queue[i]}\n`
         }
     }
 
-    menu += `\nEscolha uma opção:\n`
-    menu += `1 - Novo paciente\n`
-    menu += `2 - Consultar paciente\n`
-    menu += `3 - Sair\n`
+    menu += '\nEscolha uma opção:\n'
+    menu += '1 - Novo paciente\n'
+    menu += '2 - Consultar paciente\n'
+    menu += '3 - Sair\n'
 
-    option = prompt(menu);
+    option = prompt(menu)
 
-    if (option === `1`) {
-        let newPatient = prompt(`Digite o nome do novo paciente:`)
-        queue.push(newPatient)
-    } else if (option === `2`) {
-        if (queue.length === 0) {
-            alert(`Não há pacientes na fila.`)
-        } else {
-            let consultedPatient = queue.shift();
-            alert(`Paciente consultado: ${consultedPatient}`)
-        }
-    } else if (option !== `3`) {
-        alert(`Opção inválida!`)
+    switch (option) {
+        case '1':
+            let newPatient = prompt('Digite o nome do novo paciente:')
+            queue.push(newPatient)
+            break
+        case '2':
+            if (queue.length === 0) {
+                alert('Não há pacientes na fila.')
+            } else {
+                let consultedPatient = queue.shift()
+                alert(`Paciente consultado: ${consultedPatient}`)
+            }
+            break
+        case '3':
+            alert('Programa encerrado.')
+            break
+        default:
+            alert('Opção inválida!')
+            break
     }
 }
-
-alert(`Programa encerrado.`)
